05aa7f7ade55058f08907ccb792006737dd59ad8
NotHenryCavill <gowthamm161@gmail.com>
2023-12-12 08:57:33 +0530

    Update EmailController.java

--- src/main/java/com/example/invoiceload/EmailController.java
+++ src/main/java/com/example/invoiceload/EmailController.java
@@ -1,2 +1,24 @@
-package com.example.invoiceload;public class EmailController {
+package com.example.invoiceload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @GetMapping("/downloadAndMoveToTrash")
+    public String downloadAndMoveToTrash() {
+        try {
+            emailService.downloadAndMoveToTrash();
+            return "Emails downloaded and moved to trash successfully.";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error occurred: " + e.getMessage();
+        }
+    }
 }
